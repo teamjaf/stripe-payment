7df3d2bbe8e2176b21ab27cc38a5c0e5467a62d0
Md. Jafrin Hossain <md.jafrin.hossain@g.bracu.ac.bd>
2023-02-15 12:18:50 +0600

    ignore

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config({path:'.env'});
+}
+
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+
+//console.log(stripePublicKey, "  ", stripeSecretKey)
+
 const express = require('express')
 const app = express()
+const fs = require('fs')
+
+app.get('/store', function(req, res) {
+    fs.readFile('items.json', function(error, data) {
+      if (error) {
+        res.status(500).end()
+      } else {
+        res.render('store.ejs', {
+          stripePublicKey: stripePublicKey,
+          items: JSON.parse(data)
+        })
+      }
+    })
+})
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.listen(3000)
+app.listen(4000)
 
